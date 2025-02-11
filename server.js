16afb5d91f945780419acafad9fecf9520342510
Pankaj Pandey <107976020+mrpankajpandey@users.noreply.github.com>
2025-02-11 10:00:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import connectDB from './config/db.js';
 import router from './routes/api.js';
 dotenv.config();
 const app = express();
+app.use(express.json({ limit: '10mb' })); // Increase to 10MB
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
